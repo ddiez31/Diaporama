6420bb9d5e48615c455fd515d0d584133c35682d
David <david.diez31@free.fr>
2017-01-21 11:45:50 +0100

    last commit sans poo

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/* sans POO 
 $(document).ready(function() {
     $.getJSON("index.json", function(data) {
-
         $.each(data, function() {
             setInterval(slide, 3000);
             var i = -1;
@@ -15,35 +13,4 @@ $(document).ready(function() {
             };
         });
     });
-});
-*/
-
-// avec POO
-$(document).ready(function() {
-    $.getJSON("index.json", function(data) {
-        var liens = data.images;
-        var Diaporama = function(name, url) {
-            this.name = name;
-            this.url = url;
-            this.slide = function() {
-                setInterval(function slide() {
-                    $("img").attr('src', liens[i].url) /*.fadeIn(1000).delay(1000).fadeOut(900).delay(100)*/ ;
-                    console.log("ok");
-
-                }, 1000);
-            };
-        };
-
-        for (var i = 0; i < liens.length; i++) {
-            var diaporama1 = new Diaporama(liens[i].name, liens[i].url);
-            console.log(liens[i].url);
-            if (i === liens.length) {
-                i = 0;
-            }
-            diaporama1.slide();
-        };
-    });
-
-
-
 });
